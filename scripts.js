896232acf8afded35e5245850051be199e2f812c
Esther <estherhiuwai@student.unimelb.edu.au>
2023-05-29 15:45:14 +1000

    tried to save user info from checkout (failed)

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ function enterInfo(){
 function exitInfo(){
     document.getElementById('checkout-overall').style.display="none";
 }
+
+function saveInfo(){
+    let tbchanged = document.getElementById("address-payment");
+    let userName = document.getElementById("name").value; //this is working
+    tbchanged.innerText = userName;
+}
