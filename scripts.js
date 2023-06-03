a99663096934c8cbe8aa183e14e5e838a55d4f0b
Esther <estherhiuwai@student.unimelb.edu.au>
2023-06-03 15:38:25 +1000

    price size, pickup delivery colours, fees list line

--- scripts.js
+++ scripts.js
@@ -26,3 +26,23 @@ function saveInfo(){
     let userName = document.getElementById("name").value; //this is working
     document.getElementById("address-payment").innerHTML = userName; //this isn't working, try with a <p>
 }
+
+function activateDelivery(){
+    var active = document.getElementById("delivery");
+    active.classList.remove("pickup-delivery");
+    active.classList.add("pickup-delivery-active");
+
+    var inactive = document.getElementById("pickup");
+    inactive.classList.remove("pickup-delivery-active");
+    inactive.classList.add("pickup-delivery");
+}
+
+function activatePickup(){
+    var active = document.getElementById("pickup");
+    active.classList.remove("pickup-delivery");
+    active.classList.add("pickup-delivery-active");
+
+    var inactive = document.getElementById("delivery");
+    inactive.classList.remove("pickup-delivery-active");
+    inactive.classList.add("pickup-delivery");
+}
